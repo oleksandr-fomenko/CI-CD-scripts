b2d9d22a0b04455ed73d55242be286f172480fd0
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-14 17:43:30 +0200

    updated rp_service.py

--- ReportPortal/rp_service.py
+++ ReportPortal/rp_service.py
@@ -12,3 +12,9 @@ class RpService():
             "content-type": "application/json",
             "Authorization": f"Bearer {rp_token}"
         }
+
+    def get_ui_rp_link(self, launchId: str):
+        rp_mode_add = ""
+        if self.rp_mode == "Debug":
+            rp_mode_add = "/userdebug"
+        return f"{self.rp_domen}/ui/#{self.rp_project}{rp_mode_add}/all/{launchId}" 
