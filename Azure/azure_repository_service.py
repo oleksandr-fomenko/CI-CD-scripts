44d9a294004b6d020dab54d9904be7030e07654a
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-14 16:55:36 +0200

    updated azure_repository_service.py

--- Azure/azure_repository_service.py
+++ Azure/azure_repository_service.py
@@ -12,3 +12,17 @@ class AzureRepositoryService():
             "content-type": "application/json",
             "Authorization": f"Basic {token}"
         }
+
+    def post_comment_to_pr(self, comment: str, pr_id: str, status=4):
+        data = {
+            "comments": [
+                {
+                    "parentCommentId": 0,
+                    "content": comment,
+                    "commentType": 1
+                }
+            ],
+            "status": status
+        }
+        response = requests.post(url=f"{self.url}/pullRequests/{pr_id}/threads?api-version={self.api_version}", json=data, headers=self.headers)
+        return response
