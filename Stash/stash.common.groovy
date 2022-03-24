0792ea8dc57e0cd4f0b59f3538e6bac3f8d36916
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-24 12:42:30 +0200

    added stash pr data

--- Stash/stash.common.groovy
+++ Stash/stash.common.groovy
@@ -16,4 +16,31 @@ String STASH_REPO_SLUG() {
 	return 'your-repo-slug'
 }
 
+String STASH_PROJECT_URL() {
+	def baseUrl = STASH_URL()
+	def projectName = STASH_PROJECT_NAME()
+	def repoSlug = STASH_REPO_SLUG()
+	return "${baseUrl}/scm/${repoSlug}/${projectName}.git"
+}
+
+String STASH_PROJECT_PR_URL() {
+	def baseUrl = STASH_URL()
+	def projectName = STASH_PROJECT_NAME()
+	def repoSlug = STASH_REPO_SLUG()
+	return "${baseUrl}/rest/api/latest/projects/${repoSlug}/repos/${projectName}/pull-requests"
+}
+
+String STASH_PROJECT_PR_URL_UI_BY_ID(def prId) {
+	def baseUrl = STASH_URL()
+	def projectName = STASH_PROJECT_NAME()
+	def repoSlug = STASH_REPO_SLUG()
+	return String.format("${baseUrl}/projects/${repoSlug}/repos/${projectName}/pull-requests/%s/diff", prId)
+}
+
+String PULL_REQUEST_UI_MESSAGE(def prId){
+	def url = STASH_PROJECT_PR_URL_UI_BY_ID(prId)
+	return "Pull Request - [${url}](${url})"
+}
+
+
 return this
