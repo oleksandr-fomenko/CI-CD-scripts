5b2bb0307a6b115b43cd688d1f4d3027ca0caf2b
Oleksandr Fomenko <Oleksandr_Fomenko@epam.com>
2022-03-24 12:39:00 +0200

    added stash constants

---
+++ Stash/stash.common.groovy
@@ -0,0 +1,19 @@
+import java.util.*
+
+String STASH_CRED_ID() {
+	return 'your-stash-cred-Id'
+}
+
+String STASH_URL() {
+	return "https://your_stash_url"
+}
+
+String STASH_PROJECT_NAME() {
+	return 'your-project-name'
+}
+
+String STASH_REPO_SLUG() {
+	return 'your-repo-slug'
+}
+
+return this
